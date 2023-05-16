33a868cfcb3dda7d1ecbc9695ef5a4ba80095198
Dariia96 <darynastakhnyak@gmail.com>
2023-05-16 20:40:45 +0300

    update 3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,48 +4,37 @@ const delayInput = document.querySelector('input[name="delay"]');
 const stepInput = document.querySelector('input[name="step"]');
 const amountInput = document.querySelector('input[name="amount"]');
 const submitBtn = document.querySelector('[type="submit"]');
-const amount = amountInput.value;
-const delayValue = delayInput.value;
-
 
 function createPromise(position, delay) {
-
   const shouldResolve = Math.random() > 0.3;
-  const promiseFirst = new Promise((resolve, reject) => {
-        setTimeout(() => {
+          setTimeout(() => {
             if (shouldResolve) {
-                resolve(console.log(`✅ Fulfilled promise ${position} in ${delay}ms`))
+                resolve({ position, delay})
             } else {
     reject(console.log(`❌ Rejected promise ${position} in ${delay}ms`));
   }
         }, delay)
-    })
+    
 }
 
-
-
-submitBtn.addEventListener("click", function () {
-  createPromise(1, delayValue);
-  for (let i = 1; i < amountInput.value; i++) {
-    delay = delay + stepInput.value;
-    i = position;
-    promiseFirst
+form.addEventListener("submit", function ( event) {
+  event.preventDefault();  
+  const amount = amountInput.value;
+  const step = stepInput.value;
+  const delay = delayInput.value;
+  
+  for (let position = 0; position < amount; position++) {
+  
+    createPromise(position, delay)
     
-    
-         
-      .then(({ position, delay }) => {
-        setTimeout(() => {
+    .then(({ position, delay }) => {
+        delay = delay + step;
           if (shouldResolve) {
             console.log(`✅ Fulfilled promise ${position} in ${delay}ms`)
-                
           }
-        }, delay)
-   
       })
       .catch(({ position, delay }) => {
-        setTimeout(() => {
           console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-        });
       })
   }
   })
