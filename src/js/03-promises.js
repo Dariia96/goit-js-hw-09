55c720eb7738eda03dbecc350ba69cd5426d5204
Dariia96 <darynastakhnyak@gmail.com>
2023-05-18 18:43:19 +0300

    all tasks done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,14 +6,15 @@ const amountInput = document.querySelector('input[name="amount"]');
 const submitBtn = document.querySelector('[type="submit"]');
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {const shouldResolve = Math.random() > 0.3;
           setTimeout(() => {
             if (shouldResolve) {
                 resolve({ position, delay})
             } else {
-    reject(console.log(`❌ Rejected promise ${position} in ${delay}ms`));
+    reject({ position, delay});
   }
-        }, delay)
+        }, delay)})
+  
     
 }
 
@@ -21,17 +22,15 @@ form.addEventListener("submit", function ( event) {
   event.preventDefault();  
   const amount = amountInput.value;
   const step = stepInput.value;
-  const delay = delayInput.value;
   
-  for (let position = 0; position < amount; position++) {
   
+  for (let position = 1; position <= amount; position++) {
+  let delay = Number(delayInput.value) + Number((position - 1 ) * step) ;
     createPromise(position, delay)
     
     .then(({ position, delay }) => {
-        delay = delay + step;
-          if (shouldResolve) {
-            console.log(`✅ Fulfilled promise ${position} in ${delay}ms`)
-          }
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`)
+          
       })
       .catch(({ position, delay }) => {
           console.log(`❌ Rejected promise ${position} in ${delay}ms`);
@@ -40,15 +39,3 @@ form.addEventListener("submit", function ( event) {
   })
 
 
-//for (let i = 3; i > 0; i--) {
-// const delay = i * 1000;
-//console.log('DELAY: ' + delay);
-//console.log('i: ' + i);
-//setTimeout(() => console.log(i), delay);
-//}
- //for (let promise = 1; promise <= amount; promise++) {
- //const delay = promise * 1000;
-//console.log('DELAY: ' + delay);
-//console.log('i: ' + i);
-//setTimeout(() => console.log(), delay);
-//}
